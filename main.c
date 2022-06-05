fec4fdd914f442ee46b48e7d883a2bb3cb890db6
KieuGiap <106530335+KieuGiap@users.noreply.github.com>
2022-06-05 11:58:52 +0700

    bt tinh so buoi nghi

--- main.c
+++ main.c
@@ -3,32 +3,41 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+int main() 
+{
+	int sbh, sbn, dlt, dth, dbtl;
+	float pt_so_bn;
 
-int main(void) {
-	
-	int a,b,c;
-	
-	printf("nhap vao 3 canh tam giac\n");
-	scanf("%d%d%d",&a,&b,&c);
-	
-	if(a+b<=c)
+	printf("nhap vao sbn, sbh\n");
+	scanf("%d%d",&sbn,&sbh);
+	pt_so_bn = (sbn*100)/(sbn+sbh);
+	printf("pha tram so buoi nghi la: %.2f\n",pt_so_bn);
+	if(pt_so_bn >=25 )
 	{
-		printf("khong phai la tam giac");
-	}
-	else if (a==b&&b==c)
-	{
-		printf("tam giac deu");
-	}
-	else if( a==b ||b==c||c==a){
-	
-	printf("tam giac can");
+		printf("Hoc lai\n");
 }
+
 	else
+{
+	printf("nhap vao dlt,dth,dbtl\n");
+	scanf("%d%d%d", &dlt, &dth, &dbtl);
+	 if(dlt>=8)
+	 {
+	 	printf("PASS LT\n");
+	 }
+	else 
+	printf("fail LT\n");
+	if(dth>=6)
 	{
-		printf("tam giac BT");
+		printf("pass TH\n");
 	}
+	else
+	printf("fail TH\n");
+	if(dbtl>=4){
+		printf("pass BTL\n");
+	}
+	else
+	printf("fail BTL\n");
 }
-	
-		
-
-
+	return 0;
+}
