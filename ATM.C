0c6f67c627c5ff813a333570c5b327963bb0ed0f
KieuGiap <106530335+KieuGiap@users.noreply.github.com>
2022-06-15 19:48:49 +0700

    Bài tập ATM

---
+++ ATM.C
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int luachon;
+	char i;
+	int cardid;
+	int pin;
+	printf("login\n");
+	printf("nhap vao cardid:\n");
+	scanf("%d",&cardid);
+	printf("nhap vao pin:\n");
+	scanf("%d",&pin);
+	
+	if(cardid==123 && pin== 456)
+	{
+		do{		
+		printf("1. Rut tien\n2. chuyen khoan\n3 .xem so du\n4. ket thuc\n");
+		printf("moi ban lua chon:\n");
+		scanf("%d",&luachon);
+	switch (luachon)
+	{
+		case 1:
+			{
+				printf("1. Rut tien\n");
+				break;
+			}
+		case 2:
+			{
+				printf("2. chuyen khoan\n");
+				break;
+			
+			}
+			case 3:
+				{
+					printf("3. xem so du\n");
+					break;
+				}
+			case 4 :
+				{
+					printf("4. ket thuc\n");
+					break;
+				}
+				default :
+					{
+					printf("lua chon sai, de nghi chon lai\n");
+					break;
+				}
+		}
+		printf("ban co muon tiep tuc (y/n): ");
+		scanf("%c",&i);
+	}
+		while(i!='N');
+		printf("ket thuc \n");			
+	}
+	else
+	printf("invalid\n");
+
+	return 0;
+}
--- main10.c
+++ main10.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
-int Fibonacci(int n)
+int F(int n)
 {
-	int f1=1,f2=1;
+	int f1=1, f2=1;
 	if(n==1||n==2)
 	return 1;
-	int i=3,f;
+	int i=3, f;
 	while(i<=n)
 	{
-	f=f1+f2;
-	f1=f2;
-	f2=f;
-	i++;
+		f=f1+f2;
+		f1=f2;
+		f2=f;
+		i++;
 	}
-	return f;	
+	return f;
+
 }
 int main()
 {
-    int n;
-    printf("nhfp vao gtri n \n");
-    scanf("%d",&n);
-    printf(" gif tri thu %d la %d\n",n, Fibonacci(n));
+   int n;
+   printf("nhap n \n");
+   scanf("%d", &n);
+   printf("gia tri thu %d cua day fibonacci la %d\n",n, F(n));
     return 1;
 }
