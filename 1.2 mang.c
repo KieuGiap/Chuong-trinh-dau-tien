cf38d9ac8c68feaca8cabab2bfb1963c03d080ba
KieuGiap <106530335+KieuGiap@users.noreply.github.com>
2022-06-15 23:34:58 +0700

    11.3 mang

--- 1.2 mang.c
+++ 1.2 mang.c
@@ -4,24 +4,24 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 void main(int argc, char *argv[]) {
-	int ary[10];
+	int ary[4];
 	int i,total,high;
-	for(i=0;i<10;i++)
+	for(i=0;i<4;i++)
 	{
 		printf("\nnhap gia tri %d :",i+1);
 		scanf("%d", &ary[i]);
 		}	
 	
 	high=ary[0];
-	for(i=1;i<10;i++)
+	for(i=1;i<4;i++)
 	{
 		if(ary[i]>high)
-		high=ary[i];
-		
+		high=ary[i];	
 	}
 	printf("\n gia tri cao nhat nhap vao la %d",high);
-	for(i=0, total=0;i<10; i++){
+	for(i=0, total=0;i<4; i++){
 	total=total+ary[i];
+	}
 	printf("\n gia tri trung binh la %f",total/i);
-}
+
 }
